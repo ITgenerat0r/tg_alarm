1d7d15dbd4eee02c3e890990c068c66af35d758e
GFMws <ydalite01@gmail.com>
2024-09-30 16:41:48 +0400

    delete pass in msg

--- main.py
+++ main.py
@@ -192,7 +192,7 @@ def set_pass(message):
     else:
         db.add_user(login, sha256, name)
     # remove pass message
-    bot.delete_message(message.chat.id, message[-1].message_id)
+    bot.delete_message(message.chat.id, message.message_id-1)
     bot.send_message(message.chat.id, f"Готово!")
 
 
