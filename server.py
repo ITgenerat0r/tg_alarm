85f607d49eda318b761fdfbdf1389c7b517824cb
GFMws <ydalite01@gmail.com>
2024-10-04 12:38:09 +0400

    fix send data to user

--- server.py
+++ server.py
@@ -156,8 +156,6 @@ def handler(conn, addr):
 										x = ind
 									tx = data[:x]
 									data = data[x:]
-									prt(yellow_text(tx))
-									prt(blue_text(data))
 									bot.send_message(chat, tx)
 					except Exception as e:
 						print("Failed to send data!")
